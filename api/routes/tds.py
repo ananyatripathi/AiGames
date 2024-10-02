9f24a835966e4322404f09805389bdf1452758a0
Ananya Tripathi <72099168+ananyatripathi@users.noreply.github.com>
2024-10-02 14:51:36 +0530

    Added 'Never Have I ever Game'

--- api/routes/tds.py
+++ api/routes/tds.py
@@ -32,3 +32,19 @@ def ask_suggestion():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+@tds_bp.route('/answer-nhie', methods=['POST'])
+def ask_suggestion_nhie():
+    try:
+        nhie_data = request.get_json()
+        validated_tds_data = tds_schema.load(nhie_data)
+        rounds = validated_tds_data['rounds']
+        gender = validated_tds_data['gender']
+        age_group = validated_tds_data['age_group']
+        playing_with = validated_tds_data['playing_with']
+        user_prompt = validated_tds_data['user_prompt']
+        prompt = tds_service.get_prompt_nhie(rounds=rounds, gender=gender, age_group=age_group, playing_with=playing_with, user_prompt=user_prompt)
+        answer = tds_service.get_answer_nhie(prompt)
+        return jsonify(answer), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
